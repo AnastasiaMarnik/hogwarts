fcba4d9ca3daac09ca69e8e6e58b3062600976eb
AnastasiaMarnik <lyrapolyarnaya@mail.ru>
2023-01-06 08:16:18 +0000

    first commit

---
+++ pgConfig.js
@@ -0,0 +1,9 @@
+//Импортируем класс Pool из модуля pg
+
+import pg from "pg";
+
+
+//Создаём пул соединений с сервером.
+export const pool = new pg.Pool({connectionString: process.env.DB_URL,
+	    ssl: { rejectUnauthorized: false}
+})
